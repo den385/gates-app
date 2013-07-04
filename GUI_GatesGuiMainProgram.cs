bfcb5e74783bfc299c3249966f843e089d378858
d-sherstennikov <d-shr@yandex.ru>
2013-07-04 03:36:29 -0700

    Create GUI_GatesGuiMainProgram.cs

---
+++ GUI_GatesGuiMainProgram.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GatesGUI
+{
+  static class GatesGuiMainProgram
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new GatesGuiForm());
+    }
+  }
+  
+}
